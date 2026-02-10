8369b8daa8ca8fffcdfe38514dea23291a2e0a23
noah-zipor <noah.zipor@kds.vic.edu.au>
2026-02-10 11:35:03 +1100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+basic.show_string("Started Up!")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.showString("Started Up!")
+basic.forever(function on_forever() {
+    
+})
